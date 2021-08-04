843c04d57b07ce2157b14df9d96a2a91fa94c31c
Phineas Kibbey <phineas.kibbey@vizio.com>
2021-08-03 22:44:18 -0700

    more logs

--- src/playback-watcher.js
+++ src/playback-watcher.js
@@ -417,6 +417,10 @@ export default class PlaybackWatcher {
       this.cancelTimer_();
       this.tech_.setCurrentTime(currentTime);
 
+      this.simpleLog(`Stopped at ${currentTime} while inside a buffered region ` +
+        `[${currentRange.start(0)} -> ${currentRange.end(0)}]. Attempting to resume ` +
+        'playback by seeking to the current time.');
+
       // unknown waiting corrections may be useful for monitoring QoS
       this.tech_.trigger({type: 'usage', name: 'vhs-unknown-waiting'});
       this.tech_.trigger({type: 'usage', name: 'hls-unknown-waiting'});
